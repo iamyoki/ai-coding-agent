01d422addb198d353ea7d8efd887aa4ac8c508a9
Yoki <74389358+iamyoki@users.noreply.github.com>
2026-01-05 22:10:45 +0800

    ✨ feat: fetch ollama api

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,25 @@
-// 11
-console.log("hello world");
+// curl http://localhost:11434/api/generate -d '{
+//   "model": "gemma3",
+//   "prompt": "Why is the sky blue?"
+// }'
+
+const res = await fetch("http://localhost:11434/api/generate", {
+  method: "post",
+  body: JSON.stringify({
+    model: "qwen3:4b-instruct-2507-q4_K_M",
+    prompt: "why is the sky blue?",
+    // stream: false,
+  }),
+});
+
+if (!res.body) process.exit(0);
+
+const decoderStream = new TextDecoderStream();
+
+const stream = res.body.pipeThrough(decoderStream);
+
+for await (const chunk of stream) {
+  const json = JSON.parse(chunk);
+  process.stdout.write(json.response);
+}
+process.stdout.write("\n");
