9976aff5764ef2b802bccd4f1e6e5b2d43a5d946
Yoki <74389358+iamyoki@users.noreply.github.com>
2026-01-16 00:09:56 +0800

    💄 style: clean code

--- src/agent.ts
+++ src/agent.ts
@@ -1,5 +1,5 @@
 import { stepCountIs, ToolLoopAgent } from "ai";
-import { ollamaQwen3_4b_instruct_q4_KM, zhipuGLMModel } from "./models.ts";
+import { ollamaQwen3_4b_instruct_q4_KM } from "./models.ts";
 import { tools } from "./tools/index.ts";
 
 export const agent = new ToolLoopAgent({
